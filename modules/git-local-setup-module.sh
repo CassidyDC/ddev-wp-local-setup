c6423a70929e509be77f1c4caef657275d714072
Jacob Cassidy <jacob@jacobcassidy.com>
2025-10-18 08:58:54 +0700

    Remove .gitignore file creation

--- modules/git-local-setup-module.sh
+++ modules/git-local-setup-module.sh
@@ -9,18 +9,3 @@ else
   # Print success message
   printf "${GREEN}Local Git repository created at: ${BOLD}.git${RESET}\n\n"
 fi
-
-# Add .gitignore file to project
-printf "${BLUE}Creating .gitignore file...${RESET}\n"
-if [ -f ".gitignore" ]; then
-  printf "${BLACK}The .gitignore file already exists. Skipping creation.${RESET}\n\n"
-else
-  # Copy/Paste file
-  cp ${ROOT_DIR}/files/gitignore.txt ./.gitignore
-  # Update .gitignore allow list to include custom theme if active
-  if $INSTALL_CASSIDYDC_STARTER_THEME; then
-    sed -i '' "s|# \!wp-content/themes/theme-name|\!wp-content/themes/${CASSIDYDC_STARTER_THEME_SLUG}|g" .gitignore
-  fi
-  # Print success message
-  printf "${GREEN}File created at: ${BOLD}.gitignore${RESET}\n\n"
-fi
